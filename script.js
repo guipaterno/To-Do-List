fec7505aaeb1380ac6bdcfdbff21081f63f89495
Guilherme Paterno <guipaterno@gmail.com>
2023-06-02 18:29:57 -0300

    Commit antes de abrir o novo script
    antes da inserção do arquivo moment.js

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const $modal = document.querySelector("#modal")
 const $descriptionInput = document.querySelector("#description")
 const $priorityInput = document.querySelector("#priority")
 const $deadlineInput = document.querySelector("#deadline")
+const $todoColumnBody = document.querySelector("#todoColumn .body")
+
+var todoList = []
 
 function openModal(){
     $modal.style.display = "flex"
@@ -11,10 +14,48 @@ function closeModal(){
     $modal.style.display = "none"
 }
 
+function generateCards(){
+    const todoListHtml = todoList.map(function(task){
+        return `
+        <div class="card"> 
+         <div class="info">
+            <b>Descrição</b>
+            <span>${task.description}</span>
+         </div>
+        
+
+        <div class="info">
+            <b>Prioridade</b>
+            <span>${task.priority}</span>
+         </div>
+
+         <div class="info">
+            <b>Prazo</b>
+            <span>${task.deadLine}</span>
+         </div>
+         </div>
+        
+
+        `
+
+    })
+
+
+    $todoColumnBody.innerHTML = todoListHtml.join("")
+
+}
+
 function createTask (){
-    console.log($descriptionInput.value)
-    console.log($priorityInput.value)
-    console.log($deadlineInput.value)
+   const newTask = {
+    description: $descriptionInput.value,
+    priority: $priorityInput.value,
+    deadLine: $deadlineInput.value,
+}
+
+todoList.push(newTask)
 
     closeModal()
+    generateCards()
+
+    console.log(todoList)
 }
